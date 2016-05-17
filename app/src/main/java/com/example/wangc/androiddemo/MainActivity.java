2f983983530417b4d62fd8c54eeeaa666934dc7f
Wangc <274281610@qq.com>
2016-05-17 22:05:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wangc/androiddemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.wangc.androiddemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_view:
+                startActivity(new Intent(MainActivity.this,ViewActivity.class));
+                break;
+            case R.id.btn_view_group:
+                startActivity(new Intent(MainActivity.this,ViewGroupActivity.class));
+                break;
+            case R.id.btn_animation:
+                startActivity(new Intent(MainActivity.this,AnimationActivity.class));
+                break;
+            case R.id.btn_rx_android:
+                startActivity(new Intent(MainActivity.this,RxAndroidActivity.class));
+                break;
+            case R.id.btn_event_bus:
+                startActivity(new Intent(MainActivity.this,RetrofitActivity.class));
+                break;
+            case R.id.btn_ok_http:
+                startActivity(new Intent(MainActivity.this,OkhttpActivity.class));
+                break;
+            case R.id.btn_retrofit:
+                startActivity(new Intent(MainActivity.this,RetrofitActivity.class));
+                break;
+
+        }
+    }
+}
