aa46e308943ba7ff954d789f4ea25fb7cf205dbb
wang.cheng <WangCheng@CHEN-PC>
2016-05-19 14:01:45 +0800

    新增hotfix

--- app/src/main/java/com/example/wangc/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/wangc/androiddemo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.wangc.androiddemo;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
