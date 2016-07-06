7483e6c09404a11cd92a3dbe59d5b569649795e1
王成 <274281610@qq.com>
2016-07-06 18:57:48 +0800

    rx

--- app/src/main/java/com/example/wangc/activity/RxAndroidActivity.java
+++ app/src/main/java/com/example/wangc/activity/RxAndroidActivity.java
@@ -1,7 +1,17 @@
 package com.example.wangc.activity;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+
 /**
  * Created by Wangc on 2016/5/17
  * E-MAIL:274281610@QQ.COM
@@ -12,5 +22,29 @@ public class RxAndroidActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_android);
+
+        Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("1");
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("", "===" + s);
+            }
+        });
+
     }
 }
