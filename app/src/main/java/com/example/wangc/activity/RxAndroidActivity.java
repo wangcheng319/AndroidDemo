14797197944017c6a57f81b102705f3e85330faa
王成 <274281610@qq.com>
2016-07-15 17:07:55 +0800

    rx

--- app/src/main/java/com/example/wangc/activity/RxAndroidActivity.java
+++ app/src/main/java/com/example/wangc/activity/RxAndroidActivity.java
@@ -1,18 +1,20 @@
 package com.example.wangc.activity;
 
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
-import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 
 /**
+ * RxAndroid 操作符
+ * <p>
  * Created by Wangc on 2016/5/17
  * E-MAIL:274281610@QQ.COM
  */
@@ -23,28 +25,28 @@ public class RxAndroidActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_android);
 
-        Observable.create(new Observable.OnSubscribe<String>() {
+        /*******Observable的基础创建 ：Create********/
 
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("1");
-            }
-        }).subscribe(new Observer<String>() {
-            @Override
-            public void onCompleted() {
+        Observable.just("one", "two", "three", "four", "five")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .delay(3, TimeUnit.SECONDS)
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
 
-            }
+                    }
 
-            @Override
-            public void onError(Throwable e) {
+                    @Override
+                    public void onError(Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onNext(String s) {
-                Log.d("", "===" + s);
-            }
-        });
+                    @Override
+                    public void onNext(String s) {
+                    Log.d("","=="+s);
+                    }
+                });
 
     }
 }
