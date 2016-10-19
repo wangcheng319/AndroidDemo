6dbbaa6287828d5ce6a1cb3110735c2cca7594ae
王成 <274281610@qq.com>
2016-10-19 17:43:05 +0800

    webview js 互相调用

--- app/src/main/java/com/example/wangc/activity/OkhttpActivity.java
+++ app/src/main/java/com/example/wangc/activity/OkhttpActivity.java
@@ -2,6 +2,10 @@ package com.example.wangc.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Wangc on 2016/5/17
  * E-MAIL:274281610@QQ.COM
@@ -12,5 +16,8 @@ public class OkhttpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okhttp);
+        //test
+        System.out.println("this is test");
+        List list = new ArrayList<>();
     }
 }
--- app/src/main/java/com/example/wangc/activity/RetrofitActivity.java
+++ app/src/main/java/com/example/wangc/activity/RetrofitActivity.java
@@ -2,6 +2,9 @@ package com.example.wangc.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import retrofit2.Retrofit;
+
 /**
  * Created by Wangc on 2016/5/17
  * E-MAIL:274281610@QQ.COM
@@ -12,5 +15,11 @@ public class RetrofitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .build();
+
+
     }
 }
